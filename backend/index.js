5d149c8dec399be6457cc5122e2064110120e770
Muhammad Hassan Sheikh <hassan24sk@gmail.com>
2024-02-02 00:28:05 +0500

    fixed import name

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const GameManager = require("./GameManager");
+const GameManager = require("./gameManager");
 const path = require("path");
 
 const app = express();
